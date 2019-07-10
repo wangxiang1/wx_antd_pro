3c56fcbe0adba462d4ed860b061fad377480ddcd
wangxiang1 <wangxiang1@bonc.com.cn>
2019-07-10 11:27:12 +0800

    test

---
+++ src/routes/test/testreact.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: wangxiang 
+ * @Date: 2019-07-10 11:18:31 
+ * @Last Modified by:   wangxiang 
+ * @Last Modified time: 2019-07-10 11:18:31 
+ */
+import React, { Component } from 'react';
+
+class testreact extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default testreact;
